1f20126dfe35c3fbffe1875784688ff95d78e953
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-08-15 20:49:19 +0530

    Setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      mir
+      <Button variant="primary">Button</Button>
+      <Button variant="glass">Button</Button>
+    </div>
+  );
+}
